9e286646f4bc0a102cfdd19b9232b52798293696
sdekansky <sebastiandekansky@gmail.com>
2024-03-04 11:04:44 -0800

    deleted old homepage assets and made gallery page with functionality and new assets

--- public/jgall.js
+++ public/jgall.js
@@ -1,4 +1,3 @@
-// JavaScript for slideshow
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -6,33 +5,20 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    var texts = document.getElementsByClassName("text");
+    var numberText = document.getElementsByClassName("numbertext")[0]; // get the numbertext element
+
     if (n > slides.length) {slideIndex = 1}    
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        texts[i].style.display = "none";  
     }
     slides[slideIndex-1].style.display = "block";  
-    setTimeout(function() {
-        slides[slideIndex-1].style.opacity = 1;
-    }, 50);
-    dots[slideIndex-1].className += " active";
-}
+    texts[slideIndex-1].style.display = "block";  
+    numberText.innerHTML = slideIndex + "/" + slides.length;
+  }
 
-
-var overlays = document.getElementsByClassName("polygon-overlay");
-for (var i = 0; i < overlays.length; i++) {
-    overlays[i].addEventListener("click", function() {
-        // Display information in the information container
-    });
-}
